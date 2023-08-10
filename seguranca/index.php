370024cb6c8880f6549d96c1d393c27a37e8fb0a
Ayron Nascimento <nayrton383@gmail.com>
2023-08-10 17:44:24 -0300

    reposição de arquivos de outro repositorio

---
+++ seguranca/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;1,100;1,700&family=Poppins:wght@200&family=Raleway:ital,wght@0,100;0,200;0,300;0,400;0,500;1,100;1,200;1,300;1,400;1,500&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sympatic - Segurança no Trabalho</title>
+    <style>
+
+</style>
+</head>
+<body>
+    <header>
+    <div class="menubar">
+
+    
+<div class="image-bar">
+           
+       <a href="index.php"> <img src="imagens/logo.jfif" alt="" class="bar-image-top"></a>
+    </div>
+    <div class="text-top-div"><h1 class="title-top">Segurança no Trabalho</h1></div>
+        <ul class="menu-dropdown">
+            <li class="md-item">
+              
+
+            <li class="md-item">
+                <a href="" class="menu-dropdown-item-link"><img src="imagens/icons/iconmenu.png" alt="icone de menu" class="iconmenu"></a>
+                <ul class="submenu-dropdown">
+                    <li class="sd-item">
+                        <a href="">Eletro eletronica</a>
+                        <a href="">Solda</a>
+                        <a href="">Torno</a>
+                        <a href="">Mecanica Geral</a>
+                        <a href="">Automotiva</a>
+                </ul>
+            </li>
+        </ul>
+    </div>
+    </header>
+    <main style="height: 70vh; width: 100%;">
+
+    </main>
+<aside style="height: 50vh; width: 100%; background-color: blue;">
+
+</aside>
+    <footer style="height: 30vh; width: 100%; background-color: red;">
+<div><h1>RODAPÉ</h1></div>
+    </footer>
+</body>
+</html>
